d1957457ed6b1e1701d529f0e7cb642b1bc2c6f6
Tomas Matejka <mtk.eworkers@gmail.com>
2014-08-07 01:02:28 +0200

    Basic CRUD for Player working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,8 +7,12 @@ class PlayersController < ApplicationController
     @player = Player.new()
   end
 
+  def show
+    @player = Player.find(params[:id])
+  end
+
   def create
-    @player = Player.create(name: params[:player][:name], email: params[:player][:email])
+    @player = Player.create(player_params)
 
     unless @player.errors.empty?
       flash[:error] = 'Chyba'
@@ -18,9 +22,36 @@ class PlayersController < ApplicationController
     #@player.save
 
     if @player.save
-      redirect_to :action => 'index'
-      #else
-      #  render action: new
+      redirect_to @player
+      else
+        render 'new'
     end
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
+
+    redirect_to players_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :email)
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,7 @@
 class Player < ActiveRecord::Base
   has_many :decks
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
